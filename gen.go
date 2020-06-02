155b3b3a7235f4c5d6651edc050bbb7ec7180b9c
timestee <wanghuidev@gmail.com>
2020-06-03 00:26:57 +0800

    Options using Option as TypeName

--- gen.go
+++ gen.go
@@ -37,7 +37,8 @@ type optionField struct {
 }
 
 type templateData struct {
-	ClassOptionInfo map[string][]optionInfo
+	ClassOptionInfo     map[string][]optionInfo
+	ClassOptionTypeName map[string]string
 }
 
 type optionInfo struct {
@@ -72,14 +73,14 @@ func (g fileOptionGen) gen(optionWithStructName bool) {
 	}
 
 	tmp := templateData{
-		ClassOptionInfo: make(map[string][]optionInfo),
+		ClassOptionInfo:     make(map[string][]optionInfo),
+		ClassOptionTypeName: make(map[string]string),
 	}
 	for className, exist := range g.ClassList {
 		if exist {
 			for _, val := range g.ClassOptionFields[className] {
 				name := strings.Trim(val.Name, "\"")
-				funcName := ""
-				funcName = "With"
+				funcName := "With"
 				if optionWithStructName {
 					funcName = funcName + strings.Title(className)
 				}
@@ -96,6 +97,14 @@ func (g fileOptionGen) gen(optionWithStructName bool) {
 					Body:           template.HTML(val.Body),
 				})
 			}
+			optionTypeName := className + "Option"
+			if strings.HasSuffix(className, "Options") {
+				optionTypeName = className[:len(className)-1]
+			}
+			if strings.HasSuffix(className, "Opts") {
+				optionTypeName = className[:len(className)-1]
+			}
+			tmp.ClassOptionTypeName[className] = optionTypeName
 		}
 	}
 
@@ -133,25 +142,26 @@ func goimportsBuf(buf *bytes.Buffer) (*bytes.Buffer, error) {
 
 const templateTextWithPreviousSupport = `
 {{- range $className, $optionList := .ClassOptionInfo }}
+
 type {{ $className }} struct {
 	{{- range $index, $option := $optionList }}
 		{{ $option.Name }} {{ $option.Type }}
 	{{- end }}
 }
 
-func (cc *{{ $className }}) SetOption(opt {{$className}}Option) {
+func (cc *{{ $className }}) SetOption(opt {{index $.ClassOptionTypeName $className}}) {
 	_ = opt(cc)
 }
 
-func (cc *{{ $className }}) GetSetOption(opt {{$className}}Option) {{$className}}Option {
+func (cc *{{ $className }}) GetSetOption(opt {{index $.ClassOptionTypeName $className}}) {{index $.ClassOptionTypeName $className}} {
 	return opt(cc)
 }
 
-type {{$className}}Option func(cc *{{$className}}) {{$className}}Option
+type {{index $.ClassOptionTypeName $className}} func(cc *{{$className}}) {{index $.ClassOptionTypeName $className}}
 {{ range $index, $option := $optionList }}
 
 {{- if eq $option.GenOptionFunc true }}
-	func {{$option.OptionFuncName}}(v {{$option.Type}}) {{$className}}Option   { return func(cc *{{$className}}) {{$className}}Option {
+	func {{$option.OptionFuncName}}(v {{$option.Type}}) {{index $.ClassOptionTypeName $className}}   { return func(cc *{{$className}}) {{index $.ClassOptionTypeName $className}} {
 		previous := cc.{{$option.Name}}
 		cc.{{$option.Name}} = v
 		return {{$option.OptionFuncName}}(previous)
@@ -160,7 +170,7 @@ type {{$className}}Option func(cc *{{$className}}) {{$className}}Option
 
 {{- end }}
 
-func New{{$className}}(opts ... {{$className}}Option) *{{ $className }} {
+func New{{$className}}(opts ... {{index $.ClassOptionTypeName $className}}) *{{ $className }} {
 	cc := newDefault{{ $className }}()
 	for _, opt := range opts  {
 		_ = opt(cc)
@@ -177,7 +187,7 @@ func Install{{$className}}WatchDog(dog func(cc *{{$className}})) {
 
 var watchDog{{$className}} func(cc *{{$className}})
 
-var default{{$className}}Options = [...]{{$className}}Option {
+var default{{index $.ClassOptionTypeName $className}}s = [...]{{index $.ClassOptionTypeName $className}} {
 {{- range $index, $option := $optionList }}
 	{{- if eq $option.GenOptionFunc true }}
 		{{- if eq $option.FieldType 0 }}
@@ -202,7 +212,7 @@ func newDefault{{ $className }} () *{{ $className }} {
 {{- end }}
 	}
 
-	for _, opt := range default{{$className}}Options  {
+	for _, opt := range default{{index $.ClassOptionTypeName $className}}s  {
 		_ = opt(cc)
 	}
 
@@ -220,20 +230,20 @@ type {{ $className }} struct {
 	{{- end }}
 }
 
-func (cc *{{ $className }}) ApplyOption(opt {{$className}}Option) {
+func (cc *{{ $className }}) ApplyOption(opt {{index $.ClassOptionTypeName $className}}) {
 	opt(cc)
 }
 
-type {{$className}}Option func(cc *{{$className}})
+type {{index $.ClassOptionTypeName $className}} func(cc *{{$className}})
 {{ range $index, $option := $optionList }}
 
 {{- if eq $option.GenOptionFunc true }}
-	func {{$option.OptionFuncName}}(v {{$option.Type}}) {{$className}}Option   { return func(cc *{{$className}}) {cc.{{$option.Name}} = v } }
+	func {{$option.OptionFuncName}}(v {{$option.Type}}) {{index $.ClassOptionTypeName $className}}   { return func(cc *{{$className}}) {cc.{{$option.Name}} = v } }
 {{- end }}
 
 {{- end }}
 
-func New{{$className}}(opts ... {{$className}}Option) *{{ $className }} {
+func New{{$className}}(opts ... {{index $.ClassOptionTypeName $className}}) *{{ $className }} {
 	cc := newDefault{{ $className }}()
 	for _, opt := range opts  {
 		_ = opt(cc)
@@ -244,13 +254,13 @@ func New{{$className}}(opts ... {{$className}}Option) *{{ $className }} {
 	return cc
 }
 
-func Install{{$className}}WatchDog(dog {{$className}}Option) {
+func Install{{$className}}WatchDog(dog {{index $.ClassOptionTypeName $className}}) {
 	watchDog{{$className}} = dog
 }
 
-var watchDog{{$className}} {{$className}}Option
+var watchDog{{$className}} {{index $.ClassOptionTypeName $className}}
 
-var default{{$className}}Options = [...]{{$className}}Option {
+var default{{index $.ClassOptionTypeName $className}}s = [...]{{index $.ClassOptionTypeName $className}} {
 {{- range $index, $option := $optionList }}
 	{{- if eq $option.GenOptionFunc true }}
 		{{- if eq $option.FieldType 0 }}
@@ -275,7 +285,7 @@ func newDefault{{ $className }} () *{{ $className }} {
 {{- end }}
 	}
 
-	for _, opt := range default{{$className}}Options  {
+	for _, opt := range default{{index $.ClassOptionTypeName $className}}s  {
 		_ = opt(cc)
 	}
 
