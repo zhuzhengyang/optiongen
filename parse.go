8ed640544fd9f29d8413586a84106b30343292be
czy <czy463@163.com>
2021-07-06 16:54:38 +0800

    fix command args use. reduce parse code depth.

--- parse.go
+++ parse.go
@@ -10,6 +10,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"runtime"
@@ -19,10 +20,19 @@ import (
 
 var fset = token.NewFileSet()
 
-func inspectDir(wd string) {
-	_, ok := os.LookupEnv(gogenerate.GOFILE)
+func inspectDir(wd string) (envFile string, lineNo int) {
+	envFile, ok := os.LookupEnv(gogenerate.GOFILE)
 	if !ok {
 		log.Fatalf("env not correct; missing %v", gogenerate.GOFILE)
+	}
+
+	lineStr, ok := os.LookupEnv(gogenerate.GOLINE)
+	if !ok {
+		log.Fatalf("env not correct; missing %v", gogenerate.GOLINE)
+	}
+	lineNo, err := strconv.Atoi(lineStr)
+	if err != nil {
+		log.Fatalf("env not correct; env %v convert to int failed. %s", gogenerate.GOLINE, err)
 	}
 
 	tags := make(map[string]bool)
@@ -47,9 +57,10 @@ func inspectDir(wd string) {
 	if dirFiles == nil {
 		log.Fatalf("cannot find any files containing the %v directive", OptionGen)
 	}
-	//if dirFiles[envFile] != 1 {
-	//	log.Fatalf("expected a single occurrence of %v directive in %v. Got: %v", OptionGen, envFile, dirFiles)
-	//}
+	// if dirFiles[envFile] != 1 {
+	// 	log.Fatalf("expected a single occurrence of %v directive in %v. Got: %v", OptionGen, envFile, dirFiles)
+	// }
+	return
 }
 
 var gofmtBuf bytes.Buffer
@@ -74,186 +85,208 @@ var EnableDebug bool
 var Verbose bool
 
 func ParseDir(dir string, optionWithStructName bool) {
-	inspectDir(dir)
+	fileName, lineNo := inspectDir(dir)
 
-	pkgs, err := parser.ParseDir(fset, dir, nil, parser.ParseComments)
+	DstName := ""
+
+	// pkgs, err := parser.ParseDir(fset, dir, nil, parser.ParseComments)
+	// if err != nil {
+	// 	log.Fatalf("unable to parse %v: %v", dir, err)
+	// }
+	file, err := parser.ParseFile(fset, fileName, nil, parser.ParseComments)
 	if err != nil {
-		log.Fatalf("unable to parse %v: %v", dir, err)
+		log.Fatalf("unable to parse %v: %v", fileName, err)
+	}
+	filePath := filepath.Dir(fileName)
+
+	// for _, pkg := range pkgs {
+	// 	for filePath, file := range pkg.Files {
+	// 		if gogenerate.FileGeneratedBy(filePath, OptionGen) {
+	// 			continue
+	// 		}
+
+	var importPath []string
+	for _, imp := range file.Imports {
+		importPath = append(importPath, imp.Path.Value)
 	}
 
-	for _, pkg := range pkgs {
-		for filePath, file := range pkg.Files {
-			if gogenerate.FileGeneratedBy(filePath, OptionGen) {
+	classList := make(map[string]bool)
+	classOptionFields := make(map[string][]optionField)
+	for _, d := range file.Decls {
+		switch d := d.(type) {
+		case *ast.FuncDecl:
+			if d.Recv != nil {
 				continue
 			}
-
-			var importPath []string
-			for _, imp := range file.Imports {
-				importPath = append(importPath, imp.Path.Value)
+			// name check valid
+			if !strings.HasSuffix(d.Name.Name, optionDeclarationSuffix) {
+				continue
 			}
+			// command line check valid
+			p := fset.Position(d.Pos())
+			if p.Line != lineNo+1 {
+				continue
+			}
+			// Only allow return expr in class option declaration function
+			if len(d.Body.List) != 1 {
+				continue
+			}
+			// Only allow return one value
+			stmt, ok := d.Body.List[0].(*ast.ReturnStmt)
+			if !ok {
+				continue
+			}
+			if len(stmt.Results) != 1 {
+				continue
+			}
+			result := stmt.Results[0].(*ast.CompositeLit)
+			optionFields := make([]optionField, len(result.Elts))
+			for i, elt := range result.Elts {
+				switch elt := elt.(type) {
+				case *ast.KeyValueExpr:
+					// Option Field Name
+					key := elt.Key.(*ast.BasicLit)
+					optionFields[i].Name = key.Value
 
-			classList := make(map[string]bool)
-			classOptionFields := make(map[string][]optionField)
-			for _, d := range file.Decls {
-				switch d := d.(type) {
-				case *ast.FuncDecl:
-					if d.Recv != nil {
-						continue
-					}
-					if strings.HasSuffix(d.Name.Name, optionDeclarationSuffix) {
-						// Only allow return expr in class option declaration function
-						if len(d.Body.List) != 1 {
-							continue
+					switch value := elt.Value.(type) {
+					case *ast.FuncLit:
+						printLog("%s type:%s", optionFields[i].Name, "ast.FuncLit")
+						optionFields[i].FieldType = FieldTypeFunc
+						buf := bytes.NewBufferString("")
+						// Option func Type
+						_ = printer.Fprint(buf, fset, value.Type)
+						optionFields[i].Type = buf.String()
+
+						// Option func Body
+						buf.Reset()
+						_ = printer.Fprint(buf, fset, value.Body)
+						optionFields[i].Body = buf.String()
+					case *ast.CallExpr:
+						printLog("%s type:%s", optionFields[i].Name, "ast.CallExpr")
+						optionFields[i].FieldType = FieldTypeVar
+						buf := bytes.NewBufferString("")
+
+						// Option Variable Type
+						_ = printer.Fprint(buf, fset, value.Fun)
+						optionFields[i].Type = buf.String()
+						// Option Variable Value
+						buf.Reset()
+						_ = printer.Fprint(buf, fset, value.Args[0])
+						optionFields[i].Body = buf.String()
+					case *ast.BasicLit: // token.INT, token.FLOAT, token.IMAG, token.CHAR, or token.STRING
+						printLog("%s type:%s", optionFields[i].Name, "ast.BasicLit")
+						optionFields[i].FieldType = FieldTypeVar
+						switch value.Kind {
+						case token.INT:
+							optionFields[i].Type = "int"
+							optionFields[i].Body = value.Value
+						case token.FLOAT:
+							optionFields[i].Type = "float"
+							optionFields[i].Body = value.Value
+						case token.CHAR:
+							optionFields[i].Type = "byte"
+							optionFields[i].Body = value.Value
+						case token.STRING:
+							optionFields[i].Type = "string"
+							optionFields[i].Body = value.Value
 						}
-
-						stmt := d.Body.List[0]
-						// Only allow return one value
-						if stmt, ok := stmt.(*ast.ReturnStmt); !ok {
-							continue
-						} else {
-							if len(stmt.Results) != 1 {
-								continue
-							}
-							result := stmt.Results[0].(*ast.CompositeLit)
-							optionFields := make([]optionField, len(result.Elts))
-							for i, elt := range result.Elts {
-								switch elt := elt.(type) {
-								case *ast.KeyValueExpr:
-									// Option Field Name
-									key := elt.Key.(*ast.BasicLit)
-									optionFields[i].Name = key.Value
-
-									switch value := elt.Value.(type) {
-									case *ast.FuncLit:
-										printLog("%s type:%s", optionFields[i].Name, "ast.FuncLit")
-										optionFields[i].FieldType = FieldTypeFunc
-										buf := bytes.NewBufferString("")
-										// Option func Type
-										_ = printer.Fprint(buf, fset, value.Type)
-										optionFields[i].Type = buf.String()
-
-										// Option func Body
-										buf.Reset()
-										_ = printer.Fprint(buf, fset, value.Body)
-										optionFields[i].Body = buf.String()
-									case *ast.CallExpr:
-										printLog("%s type:%s", optionFields[i].Name, "ast.CallExpr")
-										optionFields[i].FieldType = FieldTypeVar
-										buf := bytes.NewBufferString("")
-
-										// Option Variable Type
-										_ = printer.Fprint(buf, fset, value.Fun)
-										optionFields[i].Type = buf.String()
-										// Option Variable Value
-										buf.Reset()
-										_ = printer.Fprint(buf, fset, value.Args[0])
-										optionFields[i].Body = buf.String()
-									case *ast.BasicLit: // token.INT, token.FLOAT, token.IMAG, token.CHAR, or token.STRING
-										printLog("%s type:%s", optionFields[i].Name, "ast.BasicLit")
-										optionFields[i].FieldType = FieldTypeVar
-										switch value.Kind {
-										case token.INT:
-											optionFields[i].Type = "int"
-											optionFields[i].Body = value.Value
-										case token.FLOAT:
-											optionFields[i].Type = "float"
-											optionFields[i].Body = value.Value
-										case token.CHAR:
-											optionFields[i].Type = "byte"
-											optionFields[i].Body = value.Value
-										case token.STRING:
-											optionFields[i].Type = "string"
-											optionFields[i].Body = value.Value
-										}
-									case *ast.CompositeLit:
-										printLog("%s type:%s", optionFields[i].Name, "ast.CompositeLit")
-										optionFields[i].FieldType = FieldTypeVar
-										optionFields[i].Type = gofmt(value.Type)
-										buf := bytes.NewBufferString("")
-										buf.Reset()
-										var data []string
-										for _, p := range value.Elts {
-											switch t := p.(type) {
-											case *ast.BasicLit:
-												data = append(data, t.Value)
-											case *ast.Ident:
-												if t.Name == "false" || t.Name == "true" {
-													data = append(data, t.Name)
-												}
-											case *ast.KeyValueExpr:
-												blKey, okKey := t.Key.(*ast.BasicLit)
-												blVal, okVal := t.Value.(*ast.BasicLit)
-												if !okKey || !okVal {
-													log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
-												}
-												data = append(data, fmt.Sprintf("%s:%s", blKey.Value, blVal.Value))
-											default:
-												log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
-											}
-										}
-										val := "nil"
-										if len(data) > 0 {
-											val = fmt.Sprintf("%s{%s}", optionFields[i].Type, strings.Join(data, ","))
-										}
-										optionFields[i].Body = val
-									case *ast.Ident:
-										if value.Obj == nil {
-											printLog("%s type:%s", optionFields[i].Name, "ast.Ident")
-										} else {
-											printLog("%s type:%s Object:%v Type:%v", optionFields[i].Name, "ast.Ident", value.Obj, value.Obj.Type)
-										}
-										optionFields[i].FieldType = FieldTypeVar
-										if value.Name == "false" || value.Name == "true" {
-											optionFields[i].Type = "bool"
-											optionFields[i].Body = value.Name
-											break
-										}
-										if value.Name == "nil" {
-											optionFields[i].Type = "interface{}"
-											optionFields[i].Body = value.Name
-											break
-										}
-										log.Fatalf("optionGen %s got type ast.Ident, please give some type hint like: string(DefaultValue) ", optionFields[i].Name)
-									default:
-										log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
-									}
+					case *ast.CompositeLit:
+						printLog("%s type:%s", optionFields[i].Name, "ast.CompositeLit")
+						optionFields[i].FieldType = FieldTypeVar
+						optionFields[i].Type = gofmt(value.Type)
+						buf := bytes.NewBufferString("")
+						buf.Reset()
+						var data []string
+						for _, p := range value.Elts {
+							switch t := p.(type) {
+							case *ast.BasicLit:
+								data = append(data, t.Value)
+							case *ast.Ident:
+								if t.Name == "false" || t.Name == "true" {
+									data = append(data, t.Name)
 								}
-							}
-
-							declarationClassName := strings.TrimPrefix(strings.TrimSuffix(d.Name.Name, optionDeclarationSuffix), "_")
-							classOptionFields[declarationClassName] = optionFields
-						}
-					}
-				case *ast.GenDecl:
-					if d.Tok == token.TYPE {
-						for _, spec := range d.Specs {
-							if typeSpec, ok := spec.(*ast.TypeSpec); ok {
-								classList[typeSpec.Name.Name] = false
+							case *ast.KeyValueExpr:
+								blKey, okKey := t.Key.(*ast.BasicLit)
+								blVal, okVal := t.Value.(*ast.BasicLit)
+								if !okKey || !okVal {
+									log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
+								}
+								data = append(data, fmt.Sprintf("%s:%s", blKey.Value, blVal.Value))
+							default:
+								log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
 							}
 						}
+						val := "nil"
+						if len(data) > 0 {
+							val = fmt.Sprintf("%s{%s}", optionFields[i].Type, strings.Join(data, ","))
+						}
+						optionFields[i].Body = val
+					case *ast.Ident:
+						if value.Obj == nil {
+							printLog("%s type:%s", optionFields[i].Name, "ast.Ident")
+						} else {
+							printLog("%s type:%s Object:%v Type:%v", optionFields[i].Name, "ast.Ident", value.Obj, value.Obj.Type)
+						}
+						optionFields[i].FieldType = FieldTypeVar
+						if value.Name == "false" || value.Name == "true" {
+							optionFields[i].Type = "bool"
+							optionFields[i].Body = value.Name
+							break
+						}
+						if value.Name == "nil" {
+							optionFields[i].Type = "interface{}"
+							optionFields[i].Body = value.Name
+							break
+						}
+						log.Fatalf("optionGen %s got type ast.Ident, please give some type hint like: string(DefaultValue) ", optionFields[i].Name)
+					default:
+						log.Fatalf("optionGen %s got type %s support basic types only", optionFields[i].Name, optionFields[i].Type)
 					}
 				}
 			}
 
-			for className := range classOptionFields {
-				classList[className] = true
-			}
-
-			for className, optionExist := range classList {
-				if !optionExist {
-					delete(classList, className)
-				}
-			}
-
-			g := fileOptionGen{
-				FilePath:          filePath,
-				FileName:          strings.TrimSuffix(filepath.Base(filePath), ".go"),
-				PkgName:           pkg.Name,
-				ImportPath:        importPath,
-				ClassList:         classList,
-				ClassOptionFields: classOptionFields,
-			}
-			g.gen(optionWithStructName)
+			declarationClassName := strings.TrimPrefix(strings.TrimSuffix(d.Name.Name, optionDeclarationSuffix), "_")
+			classOptionFields[declarationClassName] = optionFields
+			DstName = declarationClassName
+			// case *ast.GenDecl:
+			// 	if d.Tok == token.TYPE {
+			// 		for _, spec := range d.Specs {
+			// 			if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+			// 				classList[typeSpec.Name.Name] = false
+			// 			}
+			// 		}
+			// 	}
+		}
+		// find dst file line specify name
+		if DstName != "" {
+			break
 		}
 	}
+	if DstName == "" {
+		log.Fatalf("specify file %s line %d cannot find generate declare", fileName, lineNo+1)
+	}
+
+	// for className := range classOptionFields {
+	// 	classList[className] = true
+	// }
+	// for className, optionExist := range classList {
+	// 	if !optionExist {
+	// 		delete(classList, className)
+	// 	}
+	// }
+
+	pkgName := file.Name.Name
+	classList[DstName] = true
+
+	g := fileOptionGen{
+		FilePath:          filePath,
+		FileName:          strings.ToLower(DstName),
+		PkgName:           pkgName,
+		ImportPath:        importPath,
+		ClassList:         classList,
+		ClassOptionFields: classOptionFields,
+	}
+	g.gen(optionWithStructName)
+	// 	}
+	// }
 }
